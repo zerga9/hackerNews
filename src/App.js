cfc6d999bdd67ee0484a7ab962912f637e779c65
Gadiza Zerari <gadizaz@hotmail.com>
2018-10-04 12:01:31 +0100

    Map over object

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    objectID: 1,
+  },
+];
+
 class App extends Component {
   render() {
-    const helloWorld = "Welcome to the Road to learn React"
     return (
       <div className="App">
-        <h2>{helloWorld}</h2>
+        {list.map(function(item) {
+          return <div>{item.title}</div>
+        })}
       </div>
     );
   }
