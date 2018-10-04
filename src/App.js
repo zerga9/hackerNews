fdff7d23a7c981f4695feb9080fa7f4a642b9eee
Gadiza Zerari <gadizaz@hotmail.com>
2018-10-04 14:52:31 +0100

    Make button component

--- src/App.js
+++ src/App.js
@@ -94,12 +94,11 @@ class Table extends Component {
           <span>{item.num_comments}</span>
           <span>{item.points}</span>
           <span>
-          <button
+          <Button
             onClick={() => onDismiss(item.objectID)}
-            type="button"
             >
               Dismiss
-            </button>
+            </Button>
           </span>
         </div>
       )
@@ -108,6 +107,24 @@ class Table extends Component {
   )
 
 }
+}
+
+class Button extends Component {
+  render() {
+    const {
+      onClick,
+      className,
+      children,
+    } = this.props;
+    return (
+      <button
+        onClick={onClick}
+        className={className}
+        type="button"
+        >{children}
+      </button>
+    );
+  }
 }
 
 
