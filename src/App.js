7c6748a3fb5b2d3b9e471f71c4e17984cae4f05e
Gadiza Zerari <gadizaz@hotmail.com>
2018-10-04 13:00:31 +0100

    Use constructor and this.state to set state

--- src/App.js
+++ src/App.js
@@ -19,11 +19,21 @@ const list = [
 ];
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+
+  this.state = {
+    list: list,
+  };
+}
+
   render() {
     return (
       <div className="App">
-        {list.map(item => {
-          return (<div key={item.objectID}>
+        {this.state.list.map(item =>
+          <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
             </span>
@@ -31,8 +41,8 @@ class App extends Component {
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
           </div>
-        );
-        })}
+        )
+        }
       </div>
     );
   }
