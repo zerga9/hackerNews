97310b1dd8dc05aa298feffff2951abfffd4b11b
Gadiza Zerari <gadizaz@hotmail.com>
2018-10-04 15:33:36 +0100

    Basic styling components

--- src/App.js
+++ src/App.js
@@ -47,13 +47,15 @@ onSearchChange(event) {
   render() {
     const { searchTerm, list } = this.state;
     return (
-      <div className="App">
+      <div className="page">
+        <div className="interactions">
         <Search
           value={searchTerm}
           onChange={this.onSearchChange}
         >
           Search
         </Search>
+      </div>
         <Table
           list={list}
           pattern={searchTerm}
@@ -77,21 +79,32 @@ const Search = ({ value, onChange, children }) => {
     );
   }
 
+const largeColumn = {
+  width: '40%'
+}
 
+const midColumn = {
+  width: '30%'
+}
+
+const smallColumn = {
+  width: '10%'
+}
 const Table = ({onDismiss, pattern, list}) => {
     return(
-      <div>
+      <div className="table">
       {list.filter(isSearched(pattern)).map(item =>
-        <div key={item.objectID}>
-          <span>
+        <div key={item.objectID} className='table-row'>
+          <span style={largeColumn}>
             <a href={item.url}>{item.title}</a>
           </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
+          <span style={midColumn}>{item.author}</span>
+          <span style={smallColumn}>{item.num_comments}</span>
+          <span style={smallColumn}>{item.points}</span>
           <span>
           <Button
             onClick={() => onDismiss(item.objectID)}
+            className="button-inline"
             >
               Dismiss
             </Button>
