b0b31247a123624245777db5df9b85e83f3c595b
Gadiza Zerari <gadizaz@hotmail.com>
2018-10-04 12:13:13 +0100

    Map with arrowfunction ES6

--- src/App.js
+++ src/App.js
@@ -22,8 +22,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {list.map(function(item) {
-          return <div>{item.title}</div>
+        {list.map(item => {
+          return (<div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        );
         })}
       </div>
     );
